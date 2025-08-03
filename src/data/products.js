614f214301daeace569fb5bf1830cc60394cf924
Emmanuel Pintos <emma20911@gmail.com>
2025-08-03 13:01:45 -0300

    se agregaron img y detalles en firebase de los productos

--- src/data/products.js
+++ src/data/products.js
@@ -1,10 +1,10 @@
 const PRODUCTS = [
-  { id: "1", title: "El poder del ahora", price: 5000, image: "https://picsum.photos/200", category: "autoayuda", description: "Un libro esencial para vivir el presente." },
-  { id: "2", title: "Motívate cada día", price: 7500, image: "https://picsum.photos/200", category: "motivacion", description: "Frases y ejercicios para mantenerte motivado." },
-  { id: "3", title: "Psicología positiva", price: 4200, image: "https://picsum.photos/200", category: "psicologia", description: "Descubre cómo la psicología puede ayudarte a ser feliz." },
-  { id: "4", title: "Sé tu mejor versión", price: 11000, image: "https://picsum.photos/200", category: "autoayuda", description: "Herramientas para el crecimiento personal." },
-  { id: "5", title: "Despierta tu gigante interior", price: 8000, image: "https://picsum.photos/200", category: "motivacion", description: "Motivación para alcanzar tus metas." },
-  { id: "6", title: "Mente sana", price: 10000, image: "https://picsum.photos/200", category: "psicologia", description: "Claves para una mente saludable." },
+  { id: "1", title: "El poder del ahora", price: 5000, image:"/img/elpodeldelahora.jpeg" , category: "autoayuda", description: "Un libro esencial para vivir el presente." },
+  { id: "2", title: "Una inspiración para cada día", price: 7500, image: "/img/unainspiracionparacadadia.jpeg", category: "motivacion", description: "Frases y ejercicios para mantenerte motivado." },
+  { id: "3", title: "Psicología positiva", price: 4200, image: "/img/psicologiapositiva.jpg", category: "psicologia", description: "Descubre cómo la psicología puede ayudarte a ser feliz." },
+  { id: "4", title: "Ser la mejor versión", price: 11000, image: "/img/serlamejorversion.jpg", category: "autoayuda", description: "Herramientas para el crecimiento personal." },
+  { id: "5", title: "Despertando al gigante interior", price: 8000, image: "/img/despertandoalgiganteinterior.jpg", category: "motivacion", description: "Motivación para alcanzar tus metas." },
+  { id: "6", title: "Cambia tu mente, sana tu cuerpo", price: 10000, image: "/img/cambia tu mente sana tu cuerpo.jpg", category: "psicologia", description: "Claves para una mente saludable." },
 ];
 
 export function getProducts(category) {
